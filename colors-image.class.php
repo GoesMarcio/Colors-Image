839703304b46c4abbcb1caecccef85245616a575
Márcio Góes <marciopgoes@hotmail.com>
2020-12-30 19:57:59 -0300

    First commit

---
+++ colors-image.class.php
@@ -0,0 +1,219 @@
+<?php
+
+class ColorsImage{
+    private $image;
+    private $width;
+    private $height;
+    private $type;
+    private $image_resource;
+    private $image_array = array();
+    private $pallete = array();
+
+    public function __construct(string $image){
+        try{
+            $this->image = $image;
+            $this->image_details();
+            $this->read_image();
+            
+        }catch(Exception $e){
+            throw $e;
+        }
+    }
+
+    private function image_details(){
+        if(($this->url_exists($this->image) && (list($width, $height) = getimagesize($this->image)))){
+		    $this->width = $width;
+            $this->height = $height;
+            $this->type = exif_imagetype($this->image);
+            
+        }else{
+            throw new Exception("Error reading image");
+        }
+    }
+
+    private function url_exists($url){
+        if(file_exists($url))
+            return true;
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_NOBODY, 1);
+        curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        if($result !== false){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    private function read_image(){
+
+        switch ($this->type){
+            case IMAGETYPE_PNG:
+                $function_img = "imagecreatefrompng";
+                break;
+            case IMAGETYPE_JPEG:
+                $function_img = "imagecreatefromjpeg";
+            break;
+            case IMAGETYPE_GIF:
+                $function_img = "imagecreatefromgif";
+                break;
+            case IMAGETYPE_BMP:
+                $function_img = "imagecreatefrombmp";
+                break;
+            default:
+                throw new Exception("Unknown type image");
+                return;
+        }
+
+        $this->image_resource = $function_img($this->image);
+
+        for($i = 0; $i < $this->width; $i++){
+            for($j = 0; $j < $this->height; $j++){
+                $index = imagecolorat($this->image_resource, $i, $j);
+                $rgb = imagecolorsforindex($this->image_resource, $index);
+                $hex = self::RGBToHex($rgb["red"], $rgb["green"], $rgb["blue"]);
+                
+                $this->image_array[$j][$i] = array(
+                    "r" => $rgb["red"],
+                    "g" => $rgb["green"],
+                    "b" => $rgb["blue"],
+                );
+                
+                if(isset($this->pallete[$hex])){
+                    $this->pallete[$hex] += 1;
+                }else{
+                    $this->pallete[$hex] = 1;
+                }
+
+                arsort($this->pallete);
+            }
+        }
+    }
+
+    static public function RGBToHex($r, $g, $b){
+        $hex = "#";
+        $hex.= str_pad(dechex($r), 2, "0", STR_PAD_LEFT);
+        $hex.= str_pad(dechex($g), 2, "0", STR_PAD_LEFT);
+        $hex.= str_pad(dechex($b), 2, "0", STR_PAD_LEFT);
+
+        return strtoupper($hex);
+    }
+
+    static public function HexToRGB($hex) {
+        $hex = str_replace("#", "", $hex);
+      
+        if(strlen($hex) == 3){
+            $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+            $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+            $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+        }else{
+            $r = hexdec(substr($hex,0,2));
+            $g = hexdec(substr($hex,2,2));
+            $b = hexdec(substr($hex,4,2));
+        }
+
+        $rgb = array(
+            "r" => $r, 
+            "g" => $g,
+            "b" => $b
+        );
+ 
+        return $rgb;
+    }
+
+    public function get_image_array(){
+        return $this->image_array;
+    }
+
+    public function get_thumbnail($width, $height){
+        //TODO
+    }
+
+    public function get_width(){
+        return $this->width;
+    }
+
+    public function get_height(){
+        return $this->height;
+    }
+
+    public function get_type(){
+        return $this->type;
+    }
+
+    public function get_total_colors(){
+        return count($this->pallete);
+    }
+    
+    public function get_pallete(int $limit = null){
+        $data = array();
+        foreach($this->pallete as $key => $value){
+            $rgb = self::HexToRGB($key);
+            array_push($data, array(
+                "hex" => $key,
+                "rgb" => array(
+                    "r" => $rgb["r"], 
+                    "g" => $rgb["g"],
+                    "b" => $rgb["b"]
+                ),
+                "count" => $value
+            ));
+        }
+
+        if($limit){
+            return array_slice($data, 0, $limit);
+        }
+
+        return $data;
+    }
+
+    public function get_pallete_percentage(float $limit = null){
+        $total_colors = (float) $this->width * $this->height;
+        $hundred = (float) 100;
+        $data = array();
+
+        foreach($this->pallete as $key => $value){
+            $rgb = self::HexToRGB($key);
+            $percentage = (float) ($value * 100) / $total_colors;
+            $percentage = round($percentage, 2);
+            $hundred -= $percentage;
+            array_push($data, array(
+                "hex" => $key,
+                "rgb" => array(
+                    "r" => $rgb["r"], 
+                    "g" => $rgb["g"],
+                    "b" => $rgb["b"]
+                ),
+                "percentage" => $percentage
+            ));
+        }
+
+        $data[0]["percentage"] += round($hundred, 2);
+        
+        if($limit){
+            return array_slice($data, 0, $limit);
+        }
+
+        return $data;
+    }
+
+    public function get_prominent_colors(float $min_percentage){
+        $colors = $this->get_pallete_percentage();
+        $data = array();
+
+        foreach($colors as $color) {
+			if($color["percentage"] < $min_percentage)
+                break;
+            
+            array_push($data, $color);
+		}
+
+        return $data;
+    }
+}
---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+require_once("colors-image.class.php");
+
+try{
+    $image = new ColorsImage("images/camiseta.jpg");
+    // $image = new ColorsImage("https://marks.agency/wp-content/uploads/elementor/thumbs/logo-mark-amarelo-transparente-493x201-1-oxyg8z1ny3hgdab9gyok3eybsvbu7exoy879h3qk8w.png");
+    var_dump($image->get_total_colors());
+    var_dump($image->get_pallete());
+    // var_dump($image->get_pallete(10.0));
+    // var_dump($image->get_pallete_percentage(10.0));
+
+    // header('Content-type: image/jpeg');
+    // echo $image->get_thumbnail(50, 50);
+
+}catch(Exception $e){
+    echo $e->getMessage();
+}
